6edea60d5997485124c5060ab769c1015653e4f5
Ann Robles <annr.dev@gmail.com>
2023-02-13 19:49:42 -0500

    Added the swipe delete and edit

---
+++ app/src/main/java/com/example/product/helper/SwipeUnderlayButtonClickListener.java
@@ -0,0 +1,5 @@
+package com.example.product.helper;
+
+public interface SwipeUnderlayButtonClickListener {
+    void onClick(int position);
+}
--- app/src/main/java/com/example/product/ui/ProductListActivity.java
+++ app/src/main/java/com/example/product/ui/ProductListActivity.java
@@ -2,6 +2,7 @@ package com.example.product.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,13 +17,15 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.example.product.R;
 import com.example.product.databinding.ActivityListProductBinding;
+import com.example.product.helper.SwipeHelper;
 import com.example.product.model.Product;
 import com.example.product.viewmodel.ProductViewModel;
 import com.example.product.viewmodel.ProductViewModelFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProductListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
@@ -35,6 +38,8 @@ public class ProductListActivity extends AppCompatActivity implements SearchView
     private ProductAdapter productAdapter;
 
     private ProductViewModel productViewModel;
+
+    private SwipeHelper swipeHelper;
 
     private final ActivityResultLauncher<Intent> launcher =
             registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
@@ -84,6 +89,37 @@ public class ProductListActivity extends AppCompatActivity implements SearchView
                 recyclerView.setAdapter(productAdapter);
             }
         });
+
+        swipeHelper = new SwipeHelper(this, 300, binding.productList) {
+            @Override
+            protected void instantiateSwipeButton(RecyclerView.ViewHolder viewHolder, List<SwipeUnderlayButton> buffer) {
+                buffer.add(new SwipeUnderlayButton(ProductListActivity.this,
+                        "Delete",
+                        R.drawable.ic_delete_white,
+                        30,
+                        50,
+                        Color.parseColor("#ff3c30"),
+                        SwipeDirection.LEFT,
+                        position -> {
+                            Product product = products.get(position);
+                            productViewModel.deleteProduct(product);
+                            productAdapter.notifyItemRemoved(position);
+                        }));
+                buffer.add(new SwipeUnderlayButton(ProductListActivity.this,
+                        "Update",
+                        R.drawable.ic_update_white,
+                        30,
+                        50,
+                        Color.parseColor("#ff9502"),
+                        SwipeDirection.LEFT,
+                        position -> {
+                            Intent productDetailIntent = new Intent(ProductListActivity.this, ProductEntryActivity.class);
+                            productDetailIntent.putExtra("product", products.get(position));
+                            productDetailIntent.putExtra("saveBtn", false);
+                            startActivity(productDetailIntent);
+                        }));
+            }
+        };
     }
 
     @Override
